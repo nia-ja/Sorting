2d16393b615a8fb75348da2817329dd9dbb36e2f
Kasia Bondarava <kasia.bondarava@gmail.com>
2020-02-28 19:52:09 -0500

    Delete unnecessary comment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,7 +2,6 @@
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        # cur_index = i
         smallest_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
